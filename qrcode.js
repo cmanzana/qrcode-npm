5d9044bae4e778c1b9909a8a8d60839b2341d484
Arturo <arturo@redradix.com>
2014-06-24 12:11:16 +0200

    using Node.js Buffer capability to generate a base64 string instead of using base64EncodeOutputStream handcrafted function. I'm afraid it isn't a performance improvement :(

--- qrcode.js
+++ qrcode.js
@@ -1253,76 +1253,6 @@ exports.qrcode = function() {
 	};
 
 	//---------------------------------------------------------------------
-	// base64EncodeOutputStream
-	//---------------------------------------------------------------------
-
-	var base64EncodeOutputStream = function() {
-
-		var _buffer = 0;
-		var _buflen = 0;
-		var _length = 0;
-		var _base64 = '';
-
-		var _this = {};
-
-		var writeEncoded = function(b) {
-			_base64 += String.fromCharCode(encode(b & 0x3f) );
-		};
-
-		var encode = function(n) {
-			if (n < 0) {
-				// error.
-			} else if (n < 26) {
-				return 0x41 + n;
-			} else if (n < 52) {
-				return 0x61 + (n - 26);
-			} else if (n < 62) {
-				return 0x30 + (n - 52);
-			} else if (n == 62) {
-				return 0x2b;
-			} else if (n == 63) {
-				return 0x2f;
-			}
-			throw new Error('n:' + n);
-		};
-
-		_this.writeByte = function(n) {
-
-			_buffer = (_buffer << 8) | (n & 0xff);
-			_buflen += 8;
-			_length += 1;
-
-			while (_buflen >= 6) {
-				writeEncoded(_buffer >>> (_buflen - 6) );
-				_buflen -= 6;
-			}
-		};
-
-		_this.flush = function() {
-
-			if (_buflen > 0) {
-				writeEncoded(_buffer << (6 - _buflen) );
-				_buffer = 0;
-				_buflen = 0;
-			}
-
-			if (_length % 3 != 0) {
-				// padding
-				var padlen = 3 - _length % 3;
-				for (var i = 0; i < padlen; i += 1) {
-					_base64 += '=';
-				}
-			}
-		};
-
-		_this.toString = function() {
-			return _base64;
-		};
-
-		return _this;
-	};
-
-	//---------------------------------------------------------------------
 	// base64DecodeInputStream
 	//---------------------------------------------------------------------
 
@@ -1610,14 +1540,9 @@ exports.qrcode = function() {
 		var b = byteArrayOutputStream();
 		gif.write(b);
 
-		var base64 = base64EncodeOutputStream();
 		var bytes = b.toByteArray();
-		for (var i = 0; i < bytes.length; i += 1) {
-			base64.writeByte(bytes[i]);
-		}
-		base64.flush();
-
-    return 'data:image/gif;base64,' + base64;
+    var base64 = new Buffer(bytes);
+    return 'data:image/gif;base64,' + base64.toString('base64');
   };
 
 	var createImgTag = function(width, height, src, alt) {
